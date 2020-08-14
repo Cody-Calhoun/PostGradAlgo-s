14c165dee9ab52c39b4479f3a838b5288af3f6f8
Will Calhoun <wcodycalhoun@gmail.com>
2020-08-14 08:47:49 -0700

    Print255

---
+++ Basic13.js
@@ -0,0 +1,9 @@
+// Write a program that would print all the numbers from 1 to 255
+function Print255(){ 
+    for(let i = 1; i<=255; i++){
+        console.log(i);
+    }
+}
+    Print255();
+
+    
