850542f878fe5933dec93ab071b46eb8a237b57d
Will Calhoun <wcodycalhoun@gmail.com>
2020-08-14 08:51:44 -0700

    OnlyOdd

--- Basic13.js
+++ Basic13.js
@@ -4,6 +4,15 @@ function Print255(){
         console.log(i);
     }
 }
-    Print255();
+// Print255();
 
-    
+// Write a program that would print all the odd numbers from 1 to 1000
+function OnlyOdd(){
+    for(let i = 1; i<=100; i++){
+        if (i%2 != 0){
+            console.log(i)
+        }
+    }
+}
+
+OnlyOdd();
